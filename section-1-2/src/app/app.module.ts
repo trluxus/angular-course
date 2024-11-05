51ec2f94e84b8442ac7a97c2f4be1156be244462
Dmytro Moruha <trluxus@protonmail.com>
2024-11-05 21:06:00 +0200

    Add Modules

--- section-1-2/src/app/app.module.ts
+++ section-1-2/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent,],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
--- section-1-2/src/app/user/user.component.ts
+++ section-1-2/src/app/user/user.component.ts
@@ -1,12 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [CardComponent],
+  standalone: false,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
