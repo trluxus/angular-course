57ed45fb13c33134658c695735c276701e5a0a34
Dmytro Moruha <trluxus@protonmail.com>
2024-11-08 20:08:37 +0200

    Section 6 tasks

--- section-6/src/app/dashboard/server-status/server-status.component.ts
+++ section-6/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus!: 'online' | 'offline' | 'unknown';
+  private destroyRef = inject(DestroyRef);
+
+
+  ngOnInit() {
+    console.log('ON INIT');
+    const interval = setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
--- section-6/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ section-6/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { NewTicketComponent } from "../new-ticket/new-ticket.component";
+import { Component, input } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
