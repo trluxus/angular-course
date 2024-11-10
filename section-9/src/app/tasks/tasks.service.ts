5c6a2f54b79be7465fd0d7bbb3cf207798adf152
Dmytro Moruha <trluxus@protonmail.com>
2024-11-10 20:58:41 +0200

    Section 9 tasks

---
+++ section-9/src/app/tasks/tasks.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable, signal } from "@angular/core";
+import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN'
+    }
+
+    this.tasks.update((tasks) => [...tasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((tasks) => tasks.map((task) => task.id === taskId ? { ...task, status: newStatus } : task));
+    this.loggingService.log('CHANGED TASK STATUS TO ' + newStatus);
+  }
+}
+
+
---
+++ section-9/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { InjectionToken } from '@angular/core';
+import { TasksService } from './app/tasks/tasks.service';
+
+export const TasksServiceToken = new InjectionToken<TasksService>(
+  'tasks-service-token'
+);
+
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
+}).catch((err) => console.error(err));
